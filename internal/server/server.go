655f84458f60e95c817f70f34b8cfc490246a207
kasungamage <kasun.thaksala@gmail.com>
2023-03-04 19:57:10 +0530

    go routing setup

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,8 @@ import(
 	"flag"
 	"os"
 	"time"
+	"video-conference/internal/handlers"
+	w "video-conference/pkg/webrtc"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -12,7 +14,7 @@ import(
 )
 
 var (
-	addr = flag.String("addr ":"", os.Getenv("PORT"),"")
+	addr = flag.String("addr, ":""+os.Getenv("PORT"),"")
 	cert =  flag.String("cert","","")
 	key = flag.String("key","","")
 )
@@ -39,7 +41,27 @@ func Run() error{
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
 	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
 	app.Get("/stream/:ssuid", handlers.Stream)
-	app.Get("/stream/:ssuid/websocket",)
-	app.Get("/stream/:ssuid/chat/websocket",)
-	app.Get("/stream/:ssuid/viewer/websocket",)
+	app.Get("/stream/:ssuid/websocket", websocket.New(handlers.StreamWebsocket, websocket.Config{
+		HandshakeTimeout: 10*time.Second,}))
+	app.Get("/stream/:ssuid/chat/websocket", websocket.New(handlers.StreamChatWebsocket))
+	app.Get("/stream/:ssuid/viewer/websocket", websocket.New(handlers.StreamviewerWebsocket))
+	app.Static("/", "./assets")
+
+	w.Rooms = make(map[string*w.Room])
+	w.streams = make(map[string]*w.Room)
+	go dispatchKeyFrames()
+
+	if *cert != ""{
+		return app.ListenTLs(*addr, *cert, *key)
+	}
+	return app.Listen(*addr)
+
+}
+
+func dispatchKeyFrames(){
+	for range time.NewTicker(time.Second * 3).C{
+		for _,room :=  range w.Rooms{
+			room.Peers.DispatchKeyFrame()
+		} 
+	}
 }
